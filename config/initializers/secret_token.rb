4640f8f12fff1d1a511ee84abb0a227611f1fc82
Hodo Mohamed <hmohamed0@gmail.com>
2014-03-04 17:16:33 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reinex::Application.config.secret_key_base = '89ae7badc35834d60d3d43112c1a55f2b91d3557c36e5a383e81906352413dce4d998d36d0421dbe8b9d8b1ef2c4903219d244d5b4eba23da51468c7a9869ccf'
